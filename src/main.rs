326027996f1ddb67f532ac6489918e0da0076460
Adrien Bréfort <adrien.brefort@fr.clara.net>
2019-04-26 01:16:21 +0200

    First commit
    Let's just see if we can list RDS Instances for now.
    Also try to assume a role.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+#[macro_use(load_yaml)]
+extern crate clap;
+extern crate rusoto_core;
+extern crate rusoto_rds;
+extern crate rusoto_sts;
+
+use clap::App;
+use rusoto_core::{Region, HttpClient};
+use rusoto_sts::{StsClient, StsAssumeRoleSessionCredentialsProvider};
+use rusoto_rds::{Rds, RdsClient, DescribeDBInstancesMessage};
+
+
+fn main() {
+    // Parse CLI parameters
+    let yaml = load_yaml!("clap.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let region = matches.value_of("region").unwrap().parse::<Region>().unwrap();
+    let role = matches.value_of("role").unwrap();
+
+    // Get credentials from STS
+    let sts = StsClient::new(region.clone());
+
+    let provider = StsAssumeRoleSessionCredentialsProvider::new(
+        sts,
+        role.to_owned(),
+        "zabbix-discovery".to_owned(),
+        None, None, None, None
+    );
+
+    // Get RDS Instances
+    let rds = RdsClient::new_with(HttpClient::new().unwrap(), provider, region.clone());
+
+    let ddbi_message = DescribeDBInstancesMessage::default();
+    let dbi_message = rds.describe_db_instances(ddbi_message).sync();
+
+    // see if it works
+    println!("{:?}", dbi_message);
+}
