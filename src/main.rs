1cdd84a430c3850512248130e36804a588262dbf
Adrien Bréfort <adrien.brefort@fr.clara.net>
2019-04-26 09:51:32 +0200

    Use cached STS credentials

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[macro_use(load_yaml)]
 extern crate clap;
 extern crate rusoto_core;
+extern crate rusoto_credential;
 extern crate rusoto_rds;
 extern crate rusoto_sts;
 
 use clap::App;
 use rusoto_core::{HttpClient, Region};
+use rusoto_credential::AutoRefreshingProvider;
 use rusoto_rds::{DescribeDBInstancesMessage, Rds, RdsClient};
 use rusoto_sts::{StsAssumeRoleSessionCredentialsProvider, StsClient};
 
@@ -21,7 +23,7 @@ fn main() {
         .unwrap();
     let role = matches.value_of("role").unwrap();
 
-    // Get credentials from STS
+    // Get auto-refreshing credentials from STS
     let sts = StsClient::new(region.clone());
 
     let provider = StsAssumeRoleSessionCredentialsProvider::new(
@@ -34,8 +36,14 @@ fn main() {
         None,
     );
 
+    let auto_refreshing_provider = AutoRefreshingProvider::new(provider).unwrap();
+
     // Get RDS Instances
-    let rds = RdsClient::new_with(HttpClient::new().unwrap(), provider, region.clone());
+    let rds = RdsClient::new_with(
+        HttpClient::new().unwrap(),
+        auto_refreshing_provider,
+        region.clone(),
+    );
 
     let ddbi_message = DescribeDBInstancesMessage::default();
     let dbi_message = rds.describe_db_instances(ddbi_message).sync();
